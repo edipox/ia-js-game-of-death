3fd8bbe3ce6cb6a803044feaa6b0d79988f42e91
Edipo Vinicius da Silva <edipo@ycube.net>
2013-11-24 11:13:43 -0300

    UI changes applyed and useless libraries removed

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,5 +1,30 @@
+function wwidth(){
+  return $("#wwidth").val() || 40;
+}
+function wheight(){
+  return $("#wheight").val() || 40;
+}
+
+var Row = { new: function(){ return $("<div class='row'></div>") } }
+var Block = { new: function(){ return $("<span class='block'></span>") } }
+
+function createWorld(world, wwidth, wheight){
+  for(var i = 0; i < wheight; i++){
+    row = Row.new();
+    for(var j = 0; j < wwidth; j++){
+      row.append(Block.new())
+    } 
+    world.append(row);
+  }
+}
+
+
 $(document).ready(function(){
-    // removeRobot(function(){ 
-    //     createWorld(parseInt($("#rows").val()), parseInt($("#cols").val())/*, binds*/);
-    // });	 
+    //$(".block.alive.example").animate
+
+    $("#reset").on("click",function(){
+      var world = $("#world");
+      world.empty();
+      createWorld(world, wwidth(), wheight());
+    });
 });
